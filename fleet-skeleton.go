276d252c5bab00d71e409dc7e56ab79a11aa4bb5
blueblank <blueblank@gmail.com>
2014-08-13 15:29:15 -0400

    assets

--- fleet-skeleton.go
+++ fleet-skeleton.go
@@ -2,12 +2,16 @@ package fleet_skeleton
 
 import "github.com/thrisp/fleet"
 
+//import "lcl/fleet"
+
 func FSkeletonTest(f *fleet.Context) {
 	f.Data(200, "", []byte("fleetskeleton return value"))
 }
 
 func FskelCreate() (e *fleet.Engine) {
+	asst := &fleet.EngineAsset{Asset, AssetNames, AssetDir}
 	e = fleet.Basic()
+	e.IEngineAsset = asst
 	e.GET("/fleet/skeleton/:test", FSkeletonTest)
 	return e
 }
